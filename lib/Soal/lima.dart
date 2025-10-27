762b291fc9b51d867c428ad4887996dc04703483
abrielkarisma <101442361+abrielkarisma@users.noreply.github.com>
2025-10-27 16:39:09 +0700

    Finished_Soal5

--- lib/Soal/lima.dart
+++ lib/Soal/lima.dart
@@ -6,13 +6,55 @@ class SoalLima extends StatefulWidget {
   @override
   State<SoalLima> createState() => _SoalLimaState();
 }
-
 class _SoalLimaState extends State<SoalLima> {
   final TextEditingController isiTeks = TextEditingController();
   String hasil = "";
-  List vokal = ['a', 'i', 'u', 'e', 'o'];
-  List prima = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47];
+  String encrypt(String teks) {
+    List vokal = ['a', 'i', 'u', 'e', 'o'];
+    List<int> prima = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47];
+    String hasil = "";
+    int k = 0;
 
+    for (var x in teks.split("")) {
+      String y = x.toLowerCase();
+      if (vokal.contains(y)) {
+        int idx = vokal.indexOf(y);
+        int pindah = idx + prima[k];
+        while (pindah > 4) {
+          pindah = pindah - 5;
+        }
+        hasil += vokal[pindah];
+        k++;
+        if (k == prima.length) k = 0;
+      } else {
+        hasil += x;
+      }
+    }
+    return hasil;
+  }
+  String decrypt(String teks) {
+    List vokal = ['a', 'i', 'u', 'e', 'o'];
+    List<int> prima = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47];
+    String hasil = "";
+    int k = 0;
+
+    for (var x in teks.split("")) {
+      String y = x.toLowerCase();
+      if (vokal.contains(y)) {
+        int idx = vokal.indexOf(y);
+        int pindah = idx - prima[k];
+        while (pindah < 0) {
+          pindah = pindah + 5;
+        }
+        hasil += vokal[pindah];
+        k++;
+        if (k == prima.length) k = 0;
+      } else {
+        hasil += x;
+      }
+    }
+    return hasil;
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,23 +65,29 @@ class _SoalLimaState extends State<SoalLima> {
           children: [
             TextField(
               controller: isiTeks,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-              ),
+              decoration: const InputDecoration(border: OutlineInputBorder()),
             ),
             const SizedBox(height: 12),
             Row(
               children: [
                 Expanded(
                   child: ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      setState(() {
+                        hasil = encrypt(isiTeks.text);
+                      });
+                    },
                     child: const Text("Encrypt"),
                   ),
                 ),
                 const SizedBox(width: 12),
                 Expanded(
                   child: ElevatedButton(
-                     onPressed: () {},
+                    onPressed: () {
+                      setState(() {
+                        hasil = decrypt(isiTeks.text);
+                      });
+                    },
                     child: const Text("Decrypt"),
                   ),
                 ),
@@ -51,7 +99,7 @@ class _SoalLimaState extends State<SoalLima> {
               padding: const EdgeInsets.all(16),
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(8),
-                border: Border.all(color: Colors.blueAccent),
+                border: Border.all(color: Colors.black),
               ),
               child: Text(
                 hasil,
